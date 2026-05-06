cd625f28acd91342e0b3f4eb546c10d071bd69b8
Marion <marionagapay2001@gmail.com>
2026-05-06 15:38:59 +0800

    fix: reset the page when the query change

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,19 +4,20 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-export default function   Search({ placeholder }: { placeholder: string }) {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
 
     if (term) {
       params.set("query", term);
+      params.set("page", "1");
     } else {
       params.delete("query");
+      params.delete("page");
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
